ce87cf497458a67fcbaffe3d1ade82914ae0f445
Christoph Gohlke <cgohlke@cgohlke.com>
2026-02-19 17:24:12 -0800

    Add obffile/__init__.py

---
+++ obffile/__init__.py
@@ -0,0 +1,9 @@
+# obffile/__init__.py
+
+from .obffile import *
+from .obffile import __all__, __doc__, __version__
+
+# constants are repeated for documentation
+
+__version__ = __version__
+"""Obffile version string."""
